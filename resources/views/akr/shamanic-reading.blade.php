ce3310fc917568e34c382c4de3053da8dcbc19a7
Akihsra18 <154715861+Akihsra18@users.noreply.github.com>
2025-03-30 23:41:41 -0400

    Reapply "Merge branch 'test' into secrets-to-env-actions"
    This reverts commit c92e0cd238c34b0844df47c7cbe0ef0c16e025e0.

---
+++ resources/views/akr/shamanic-reading.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.app')
+
+@section('title', 'Shamanic Reading')
+
+@section('content')
+
+        <div class="container-fluid parallax_SR_1 d-flex g-0">
+            <div class="transbox col-12">
+                <div class="col-xl-7 col-md-9 col-11 transbox_2 mx-auto p-5">
+                    <div class="row align-items-center text-center h-100">
+                        <p class="title_perso fw-bold">Shamanic Reading</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid parallax_SR_2 d-flex g-0">
+            <div class="transbox col-12">
+                <div class="col-xl-7 col-md-9 col-11 transbox_3 mx-auto p-5">
+                    <div class="row align-items-center h-100">
+                        <div class="col-xl-8 col-md-10 col-12 mx-auto">
+                            <h2 class="h_texte">Shape Your Life Experiences and Begin Living a Life of Spiritual Clarity, Energetic Oneness, and Personal Empowerment</h2>
+                            <br>
+                            <p class="p_texte">
+                                Shamans believe that people should have a relationship with the spirit in all things. 
+                                They use guidance from the spirit world to shape their life experiences. They believe 
+                                that to begin living a life of spiritual clarity, energetic oneness, and personal empowerment, 
+                                you have to understand the story of your fate, destiny, and free will. This will allow you to 
+                                chart a course to live a life of deep purpose, true prosperity, and vibrant love.
+                            </p>
+                            <p class="p_texte">
+                                With the Shamanic reading, you will discover the actual situation in your life that 
+                                hinder or assist you along your path of self-awareness and healing. And you will also 
+                                know about the unique way to explore your life's journey and gain wisdom to find the magic 
+                                and meaning in the story of your life.
+                            </p>
+                            <p class="p_texte">
+                                If you want to have a shamanic reading to begin living a life of spiritual clarity, 
+                                energetic oneness, and personal empowerment, then you're at the right place.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid parallax_SR_1 d-flex g-0">
+            <div class="transbox col-12">
+                <div class="col-xl-7 col-md-9 col-11 transbox_3 mx-auto p-5">
+                    <div class="row align-items-center h-100">
+                        <div class="col-xl-8 col-md-10 col-12 mx-auto">
+                            <h2 class="h_texte">How to Proceed</h2>
+                            <br>
+                            <p class="p_texte">
+                                To order for a shamanic reading, you will have to send me an email at 
+                                <a class="p_texte text-decoration-none text-break" href="mailto:hannah.angelicconnection@gmail.com">hannah.angelicconnection@gmail.com</a> 
+                                with your personal information, and you will receive an email on how to proceed with 
+                                the payment through PayPal. The fee is 39 USD.
+                            </p>
+                            <p class="p_texte">
+                                The report for the shamanic reading is delivered in PDF report via email within 5 days after payment.
+                            </p>
+                            <br>
+                            <p class="p_texte font-monospace">
+                                Please note:
+                            </p>
+                            <ol class="p_texte">
+                                <li>By ordering a shamanic reading, you consent to me to call your angels or spirit guides for help and guidance.</li>
+                                <li>All information shared during the reading will be kept strictly confidential.</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+@endsection
